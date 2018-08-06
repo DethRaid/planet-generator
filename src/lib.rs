6c465b5f0bf9e42300b5726d720bfbc8de07a7e3
David Dubois <dexcelstraun7@gmail.com>
2018-08-05 20:05:54 -0700

    MOAR DOCS

--- src/lib.rs
+++ src/lib.rs
@@ -1,51 +1,53 @@
-/*!
- * \brief A planet that gets procedurally generated
- *
- * When a new procedural planet is spawned, the generation happens on a separate thread. Simple parameters such as 
- * distance from the planet's star and basic shape are generated immediately so that the player can see something 
- * useful, the the complex planet generation happens on a separate thread. 
- *
- * What is that generation? Wellllllllll...
- *
- * First, I look at the basic parameters. Things such as distance from the sun, gaseousness, etc are taken into 
- * consideration. Gassy planets are just gassy, they get some spin and that's that. They have a small core, but that's
- * pretty boring and maybe it's a liquid and liquid are hard to land on - so I don't worry too much about that
- *
- * Solid planets are more interesting. Based on their size, I calcualte gravitational heating and the longevity of 
- * their cores - which, combined with the age of the planet, informs the amount of atmosphere the planet gets. I 
- * simulate basic weather patterns for the duration of the atmosphere's lifetime - oceans (low areas) heat up slower
- * than the land (or vice versa - will confirm), which informs wind patterns, which informs cloud patterns - although I 
- * also consider mountains and other terrain features blocking clouds/rainfall. Then I simulate rainfall, which 
- * determines weathering and biomes
- *
- * While I'm simulating weather, I'm also simulating plate techtonics. This is all done iteratively (will determine 
- * perfect time increment for maximum planet quality vs generation time) like in real life. Basicaly, plate techtonics
- * is simulated by generating plates - randomly, although imma try to keep the plate size consistent across planet 
- * sizes - then plates are assigned a movement direction randomly (unless research comes up with a better answer). 
- * They're moved along their movement direction, which may result in new faults if a plate is pulled in multiple 
- * directions. Subduction/sublimation/whatever it's called when mountains form is fully simulated with each iteration,
- * so that the terrain can inform weather patterns and whatnot. The weather patterns can and will changes as a planet 
- * changes
- *
- * Rainfall and latitude determines biome. Earth is used as a model since we have literally no other data points for 
- * this sort of thing. Fauna and flora are based on what one might expect on Earth - areas closer to the equator have 
- * more colorful flota and fauna, colors get more muted near the poles. None of this "the whole planet is a single 
- * biome" bs - going from one pole to another should have lots of interesting variation, as should circumnavigating a 
- * planet. If I get bored enough I may try to simulate basic evolution, with flora and fauna randomly gaining or 
- * losing traits, but that's a ways off (if at all)
- *
- * At this point we can (maybe) place a civilization on this planet. Civilizations are largely based on what generates
- * interesting planets. There's various probabilities of reaching each stage of evolution - plants, fish, reptiles, 
- * mammals and birds, bipeds, Type I civilization, Type II, Type III... etc. Type I civs and later are pretty 
- * interesting because they (like our human civilization) have a lot of chances to fail. They could fail to climate 
- * change or war, and each has a different impact on the planet. More climate change means more extreme weather on the 
- * planet, whilce war leaves behind scars upon the terrain. These scars are based on the tech level of the 
- * civilization, with Type I leaving behind irradiated craters from nuclear bombs, Type II creating gorges that reach 
- * the lower crust, and Type III shattering entier planets.
- *
- * Every time a civilization falls, there's chance for another one to rise again. Each fallen civilization halves the 
- * time until the next civilization rises - a civilization builds upon its predecessors
- */
+/// The functions in this file generate a planet, psuedo-randomly. While realism is a strong concern, I have made many
+/// assumptions where little or no real-world data was available, and have broken from reality a bit to generate 
+/// intersting, unique planets. E.G. IRL a gas giant in one solar system has little to no effect on the possibility of
+/// a gas giant in a far-off solar system, but 
+/// 
+/// When a new procedural planet is spawned, the generation happens on a separate thread. Simple parameters such as 
+/// distance from the planet's star and basic shape are generated immediately so that the player can see something 
+/// useful, the the complex planet generation happens on a separate thread. 
+/// 
+/// What is that generation? Wellllllllll...
+/// 
+/// First, I look at the basic parameters. Things such as distance from the sun, gaseousness, etc are taken into 
+/// consideration. Gassy planets are just gassy, they get some spin and that's that. They have a small core, but that's
+/// pretty boring and maybe it's a liquid and liquid are hard to land on - so I don't worry too much about that
+/// 
+/// Solid planets are more interesting. Based on their size, I calcualte gravitational heating and the longevity of 
+/// their cores - which, combined with the age of the planet, informs the amount of atmosphere the planet gets. I 
+/// simulate basic weather patterns for the duration of the atmosphere's lifetime - oceans (low areas) heat up slower
+/// than the land (or vice versa - will confirm), which informs wind patterns, which informs cloud patterns - although I 
+/// also consider mountains and other terrain features blocking clouds/rainfall. Then I simulate rainfall, which 
+/// determines weathering and biomes
+/// 
+/// While I'm simulating weather, I'm also simulating plate techtonics. This is all done iteratively (will determine 
+/// perfect time increment for maximum planet quality vs generation time) like in real life. Basicaly, plate techtonics
+/// is simulated by generating plates - randomly, although imma try to keep the plate size consistent across planet 
+/// sizes - then plates are assigned a movement direction randomly (unless research comes up with a better answer). 
+/// They're moved along their movement direction, which may result in new faults if a plate is pulled in multiple 
+/// directions. Subduction/sublimation/whatever it's called when mountains form is fully simulated with each iteration,
+/// so that the terrain can inform weather patterns and whatnot. The weather patterns can and will changes as a planet 
+/// changes
+/// 
+/// Rainfall and latitude determines biome. Earth is used as a model since we have literally no other data points for 
+/// this sort of thing. Fauna and flora are based on what one might expect on Earth - areas closer to the equator have 
+/// more colorful flora and fauna, colors get more muted near the poles. None of this "the whole planet is a single 
+/// biome" bs - going from one pole to another should have lots of interesting variation, as should circumnavigating a 
+/// planet. If I get bored enough I may try to simulate basic evolution, with flora and fauna randomly gaining or 
+/// losing traits, but that's a ways off (if at all)
+/// 
+/// At this point we can (maybe) place a civilization on this planet. Civilizations are largely based on what generates
+/// interesting planets. There's various probabilities of reaching each stage of evolution - plants, fish, reptiles, 
+/// mammals and birds, bipeds, Type I civilization, Type II, Type III... etc. Type I civs and later are pretty 
+/// interesting because they (like our human civilization) have a lot of chances to fail. They could fail to climate 
+/// change or war, and each has a different impact on the planet. More climate change means more extreme weather on the 
+/// planet, whilce war leaves behind scars upon the terrain. These scars are based on the tech level of the 
+/// civilization, with Type I leaving behind irradiated craters from nuclear bombs, Type II creating gorges that reach 
+/// the lower crust, and Type III shattering entier planets.
+/// 
+/// Every time a civilization falls, there's chance for another one to rise again. Each fallen civilization halves the 
+/// time until the next civilization rises - a civilization builds upon its predecessors
+/// 
 
 #[no_mangle]
 pub extern "C" fn gen_planet() {
--- src/planet_bootstrap.rs
+++ src/planet_bootstrap.rs
@@ -1,39 +1,59 @@
-/*! 
- * \brief Generates the initial values for the planets: size, distance from sun, composition, etc
- */
+/// Generates the initial values for the planets: size, distance from sun, composition, etc
 
-/*!
- * \brief The very initial parameters for planet generation. Essentially the seed for the planet generation algorithm
- */
+/// All the elements that a planet can be made out of
+enum PlanetElement {
+    Hydrogen,
+    Helium,
+    Carbon,
+    Water,
+    Methane,
+    Ammonia,
+    Iron,
+    Silica,
+    Nitrogen,
+    SulfurDioxide,
+    Argon,
+    Neon,
+},
+
+/// A layer in a planet. ALl parts of the planet, including its atmosphere, are layers
+struct PlanetLayer {
+    /// The elemental(ish) composition of this layer. 
+    pub composition : HashMap<PlanetElement, f32>;
+
+    /// The distance from the planet's center, in meters, where the layer starts
+    pub start_height : u64;
+
+    /// The distance from the planet's center, in meters, where this layer ends
+    pub end_height : u64;
+}
+
+/// The very initial parameters for planet generation. Essentially the seed for the planet generation algorithm
 struct InitialPlanetParams {
-    /*!
-     * \brief The distance, in meters, from the planet to its star. Ranges from 0 to 10 billion, which mimics Earth's 
-     * solar system
-     */
+    /// The distance, in meters, from the planet to its star. Ranges from 0 to 10 billion, which mimics Earth's solar
+    /// system
     pub distance_from_sun : u64 = 0;
 
-    /*!
-     * \brief The age of the planet when the player finds it, in Earth years. Ranges from one thousand to thirteen 
-     * billion - Can't have planets older than the universe!
-     */
+    pub orbital_period : f32 = 0;
+
+    /// The age of the planet when the player finds it, in Earth years. Ranges from one thousand to thirteen billion 
+    /// - Can't have planets older than the universe!
     pub age : u64 = 0;
 
-    /*!
-     * \brief Whether to generate this planet as a gas giant
-     *
-     * Planets smaller than a certain size can't be gaseous because they don't have the necessary gravity to hold a
-     * thick hydrogen and helium atmosphere, so this field is tied to planetary mass - but it's not directly dependent 
-     * on it
-     */
+    /// Whether to generate this planet as a gas giant
+    /// 
+    /// Planets smaller than a certain size can't be gaseous because they don't have the necessary gravity to hold a 
+    /// thick hydrogen and helium atmosphere, so this field is tied to planetary mass - but it's not directly 
+    /// dependent on it
     pub is_gas_giant : bool = false;
 
-    /*!
-     * \brief The radius of the planet, in meters. Ranges from one million meters - about the radius of pluto - to 
-     */
+    /// The radius of the planet, in meters. Ranges from one million meters - about the radius of Pluto - to 700 
+    /// million meters, about ten times the radius of Jupiter
     pub radius : u64 = 0;
 
-    /*!
-     * \brief The mass of the planet, in meters
-     */
+    /// The mass of the planet, in meters
     pub mass : u64 = 0;
+
+    /// All the layers that compose this planet
+    pub layers : Vec<PlanetLayer>;
 };
