c151ec2a4294a183170662455f8d8902b2ed535f
David Dubois <dexcelstraun7@gmail.com>
2018-08-11 18:01:01 -0700

    icosphere generation works now

--- planet-gen/src/icosphere.rs
+++ planet-gen/src/icosphere.rs
@@ -9,66 +9,75 @@ const X: f32 = 0.525731112119133606;
 const Z: f32 = 0.850650808352039932;
 const N: f32 = 0.0;
 
-const vertices: Vec<Vector3<f32>> =
+const original_vertices: Vec<Vector3<f32>> =
 vec![
     Vector3::new(-X, N, Z), Vector3::new(X, N,  Z), Vector3::new(-X,  N, -Z), Vector3::new(X,  N, -Z),
     Vector3::new(N, Z, X), Vector3::new(N, Z, -X), Vector3::new(N, -Z,   X), Vector3::new(N, -Z, -X),
     Vector3::new(Z, X, N), Vector3::new(-Z, X, N), Vector3::new(Z, -X,   N), Vector3::new(-Z, -X,  N)
 ];
 
-const triangles: Vec<Vector3<f32>> =
+const original_triangles: Vec<[usize; 3]> =
 vec![
-    Vector3::new(0,  4,  1), Vector3::new(0, 9,  4), Vector3::new(9,  5, 4), Vector3::new(4, 5, 8), Vector3::new(4, 8,  1),
-    Vector3::new(8, 10,  1), Vector3::new(8, 3, 10), Vector3::new(5,  3, 8), Vector3::new(5, 2, 3), Vector3::new(2, 7,  3),
-    Vector3::new(7, 10,  3), Vector3::new(7, 6, 10), Vector3::new(7, 11, 6), Vector3::new(11, 0, 6), Vector3::new(0, 1,  6),
-    Vector3::new(6,  1, 10), Vector3::new(9, 0, 11), Vector3::new(9, 11, 2), Vector3::new(9, 2, 5), Vector3::new(7, 2, 11)
+    [0,  4,  1], [0, 9,  4], [9,  5, 4], [4, 5,  8], [4, 8,  1],
+    [8, 10,  1], [8, 3, 10], [5,  3, 8], [5, 2,  3], [2, 7,  3],
+    [7, 10,  3], [7, 6, 10], [7, 11, 6], [11, 0, 6], [0, 1,  6],
+    [6,  1, 10], [9, 0, 11], [9, 11, 2], [9, 2,  5], [7, 2, 11]
 ];
 
-fn vertex_for_edge(lookup: HashMap<(u32, u32), u32>, vertices: Vec<Vector3<f32>>, first: u32, second: u32) -> u32 {
+fn vertex_for_edge(lookup: HashMap<(usize, usize), usize>, vertices: Vec<Vector3<f32>>, first: usize, second: usize) -> usize {
     let mut key = (first, second);
 
-    if(key.0 > key.1) {
+    if key.0 > key.1 {
         key = (key.1, key.0);
     }
 
-    let inserted = lookup.insert(key, vertices.Num());
-    if(inserted.second) {
-        let edge0 = vertices[first];
-        let edge1 = vertices[second];
-        let point = (edge0 + edge1);
-        point.Normalize();
-        vertices.Add(point);
+    let inserted = lookup.insert(key, vertices.len());
+    let mut ret_val: usize = 0;
+
+    match inserted {
+        None => {
+            ret_val = lookup[&key];
+        },
+        Some(x) =>  {
+            let edge0 = vertices[first];
+            let edge1 = vertices[second];
+            let point = normalize(edge0 + edge1);
+            vertices.push(point);
+
+            ret_val = x;
+        }
     }
 
-    return inserted.first->second;
+    ret_val
 }
 
-// TriangleList subdivide(VertexList& vertices, TriangleList triangles) {
-//     std::unordered_map<std::pair<int32_t, int32_t>, int32_t> lookup;
-//     TriangleList result;
-// 
-//     for(auto&& each : triangles) {
-//         int32_t mid[3];
-//         for(int edge = 0; edge<3; ++edge) {
-//             mid[edge] = vertex_for_edge(lookup, vertices, each.vertex[edge], each.vertex[(edge + 1) % 3]);
-//         }
-// 
-//         result.Add({ each.vertex[0], mid[0], mid[2] });
-//         result.Add({ each.vertex[1], mid[1], mid[0] });
-//         result.Add({ each.vertex[2], mid[2], mid[1] });
-//         result.Add({ mid[0], mid[1], mid[2] });
-//     }
-// 
-//     return result;
-// }
+fn subdivide(vertices: Vec<Vector3<f32>>, triangles: Vec<[usize; 3]>) -> Vec<[usize; 3]> {
+    let lookup: HashMap<(usize, usize), usize>;
 
-pub fn make_icosphere(subdivisions: u32) -> (Vec<Vector3<f32>>, Vec<u32>) {
-    let mut mesh_vertices = vertices;
-    let mut mesh_triangles = triangles;
+    let result: Vec<[usize; 3]>;
+
+    for each in triangles {
+        let mut mid: [usize; 3];
+        for edge in 0..3 {
+            mid[edge] = vertex_for_edge(lookup, vertices, each[edge], each[(edge + 1) % 3]);
+        }
+
+        result.push([each[0], mid[0], mid[2]]);
+        result.push([each[1], mid[1], mid[0]]);
+        result.push([each[2], mid[2], mid[1]]);
+        result.push([ mid[0], mid[1], mid[2]]);
+    }
+
+    return result;
+}
+
+pub fn make_icosphere(subdivisions: u32) -> (Vec<Vector3<f32>>, Vec<[usize; 3]>) {
+    let mut mesh_vertices = original_vertices;
+    let mut mesh_triangles = original_triangles;
 
     for i in 0..subdivisions {
         mesh_triangles = subdivide(mesh_vertices, mesh_triangles);
     }
 
-    (vertices, triangles);
+    (mesh_vertices, mesh_triangles)
 }
